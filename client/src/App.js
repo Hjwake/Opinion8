5115c5b480c9a2bdc67ca3951cb2ec317a789e74
Harry Wake <harry.wake@outlook.com>
2024-04-06 17:07:38 +0100

    Create API - part 2
    Added functionality so that users can be created and added to my local database.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
+import {useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
 
-  const apiCall = () => {
-    axios.get("http://localhost:8080").then((data) => {
-      console.log(data);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = e => {
+    e.preventDefault()
+    axios.post("http://localhost:8080/signup", {username: username, password: password})
+    .then((data) => {
+      console.log(data)
+      setUsername('')
+      setPassword('')
     })
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-
-        <button onClick={apiCall}>Make API call</button>
-
-      </header>
+      <form onSubmit={submitHandler}>
+        <h3>SignUp</h3>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' value={username} onChange={(e) => setUsername(e.target.value)}/ >
+        <label htmlFor='password'>Password</label>
+        <input id='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/ >
+        <div>
+          <button type="button">Cancel</button>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require('body-parser');
+require('dotenv').config()
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(cors());
+ 
+const db = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: `${process.env.REACT_APP_DATABASE_PASSWORD}`,
+    database: 'opinion8'
+})
 
-app.get("/", (req, res) => {
-    res.send("Test.");
+app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send({username: username})
+        }
+    });
 })
 
 app.listen(8080, () => {
